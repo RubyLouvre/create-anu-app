174a68f1aca7c4428fc329c6fe774a8690963b2e
rubylouvre <cheng19840218@gmail.com>
2018-03-18 18:38:31 +0800

    修正命令

--- anu-demo/webpack.config.js
+++ anu-demo/webpack.config.js
@@ -1,14 +1,21 @@
-const path = require('path');
-
+const path = require('path')
+console.log('输出', path.resolve(__dirname, 'dist'))
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            { test: /\.js$/, use: 'babel-loader' },
-        ]
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader',exclude: /node_modules/ }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    alias: {
+      react: 'anujs',
+      'react-dom': 'anujs'
     }
-};
+  }
+}
--- index.js
+++ index.js
@@ -1,95 +1,89 @@
 #!/usr/bin/env node
-const readline = require('readline');
-const json = require('./package.json');
-const fs = require('fs');
-const fse = require('fs-extra');
-const Promise2 = require("bluebird");
+const readline = require('readline')
+const json = require('./package.json')
+const fs = require('fs')
+const fse = require('fs-extra')
 const path = require('path')
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-const webpack = require('webpack'); //访问 webpack 运行时(runtime)
-const cliPath = process.cwd();
+const cp = require('child_process')
 
-console.log("欢迎使用anu!!!!")
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+const webpack = require('webpack'); // 访问 webpack 运行时(runtime)
+
+const cliPath = process.cwd()
+
+console.log('欢迎使用anu!!!!')
 var args = process.argv.slice(2)
 switch (args) {
-    case "-h":
-        console.log("输出答案")
-        break
-    case "-v":
-        console.log("当前版本是" + json.version)
-        break
-    default:
-        var projectName, ie = args[1] == "--ie"
-        if (!args[0]) {
-            rl.setPrompt("请输入项目名")
-            rl.prompt()
-            rl.on('line', (line) => {
-                if (fs.existsSync(line.trim())) {
-                    console.log("当前目录下已经存在此文件或文件夹名，请改一个新名字")
-                    rl.prompt();
-                } else {
-                    projectName = line.trim()
-                    rl.close();
-                }
-            }).on('close', () => {
-                buildProject(path.resolve(cliPath, projectName), ie)
-                process.exit(0);
-            });
+  case '-h':
+    console.log('输出答案')
+    break
+  case '-v':
+    console.log('当前版本是' + json.version)
+    break
+  default:
+    var projectName, ie = args[1] == '--ie'
+    if (!args[0]) {
+      rl.setPrompt('请输入项目名')
+      rl.prompt()
+      rl.on('line', (line) => {
+        if (fs.existsSync(line.trim())) {
+          console.log('当前目录下已经存在此文件或文件夹名，请改一个新名字')
+          rl.prompt()
         } else {
-            buildProject(path.resolve(cliPath, args[0]), ie)
+          projectName = line.trim()
+          rl.close()
         }
-        break
-}
-
-function buildProject(projectName, supportIE) {
-    fse.ensureDir(projectName, function (e) {
-        if (e) {
-            console.log(e)
-        } else {
-            console.log('复制大量依赖包，请等待....');
-            generateStructure("anu-demo", projectName)
-                .then(() => {
-                    callWebpack(projectName)
-                })
-                .catch(function (err) {
-                    if (err) return console.error(err)
-                });
-            generateStructure("node_modules", projectName + "/node_modules")
-                .then(() => {
-                    callWebpack(projectName)
-                })
-                .catch(function (err) {
-                    if (err) return console.error(err, 2)
-                })
-
-        }
-    })
-
-}
-var i = 0
-function callWebpack(projectName) {
-    i++
-    if (i === 2) {
-
-        let configuration = require(path.resolve(projectName ,'./webpack.config.js'));
-        console.log('config', configuration)
-        let compiler = webpack(configuration);
-        //compiler.apply(new webpack.ProgressPlugin());
-
-        compiler.run(function (err, stats) {
-            console.log("执行完webpack")
-        });
-        
+      }).on('close', () => {
+        buildProject(path.resolve(cliPath, projectName), ie)
+        process.exit(0)
+      })
+    } else {
+      buildProject(path.resolve(cliPath, args[0]), ie)
     }
+    break
 }
 
-function generateStructure(demoDir, project) {
-    console.log(__dirname, "复制。。。。。")
-    return Promise2.promisifyAll(fse).copyAsync(__dirname + '/' + demoDir,
-        project, { clobber: true })
+function buildProject (pathName, supportIE) {
+  fse.ensureDir(pathName, function (error) {
+    if (error) {
+      catchError(error)
+    } else {
+      fse.copy(__dirname + '/anu-demo', pathName).then(() => {
+        process.chdir(pathName)
+        cp.exec('npm i', {cwd: pathName}, function (error, stdout, stderr) {
+          if (error) {
+            catchError(error)
+          }else {
+            console.log('当前执行路径', process.cwd(), pathName)
+            let configuration = require(path.resolve(pathName, './webpack.config.js'))
+            webpack(configuration, function (err, stats) {
+              console.log('执行完webpack',err, stats)
+            })
+          /*
+           cp.exec('webpack', {cwd: pathName}, function (error, stdout, stderr) {
+             if (error) {
+               catchError(error)
+             }else {
+               console.log('执行完webpack')
+             }
+           })
+           */
+          }
+        }).on('close', function (code) {
+          if (code !== 0) {
+            console.log(`ps 进程退出码：${code}`)
+          }else {
+            catchError('搞定')
+          }
+        })
+      })
+        .catch(catchError)
+    }
+  })
 }
-
-
+function catchError (err) {
+  if (err) return console.error(err)
+}
