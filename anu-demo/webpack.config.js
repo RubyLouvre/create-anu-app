9737192d8a006e72249dd620237702fedcb4664f
RubyLouvre <1669866773@qq.com>
2018-03-19 11:20:07 +0800

    成功

--- anu-demo/webpack.config.js
+++ anu-demo/webpack.config.js
@@ -1,25 +1,33 @@
 const path = require('path')
+const webpack = require('webpack')
+
 console.log('输出', path.resolve(__dirname, 'dist'))
+const ROOT_PATH = path.resolve(__dirname);
+
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  'mode': 'development',
-  module: {
-    rules: [
-      { test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules|vendor|bootstrap/
-      }
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    'mode': 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules|vendor|bootstrap/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            react: 'anujs',
+            'react-dom': 'anujs'
+        }
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
     ]
-  },
-  resolve: {
-    extensions: [ '.js', '.jsx'],
-    alias: {
-      react: 'anujs',
-      'react-dom': 'anujs'
-    }
-  }
 }
--- index.js
+++ index.js
@@ -7,18 +7,19 @@ const cp = require('child_process')
 const webpack = require('webpack'); // 访问 webpack 运行时(runtime)
 const cliPath = process.cwd()
 const json = require('./package.json')
+const WebpackDevServer = require("webpack-dev-server");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
-function catchError (err) {
+function catchError(err) {
   if (err) return console.error(err)
 }
 
 console.log('欢迎使用anu!!!!')
-var args = process.argv.slice(2)
+let args = process.argv.slice(2)
 switch (args) {
   case '-h':
     console.log(`
@@ -31,7 +32,7 @@ switch (args) {
     console.log('当前版本是' + json.version)
     break
   default:
-    var projectName, ie = args[1] == '--ie'
+    let projectName, ie = args[1] == '--ie'
     if (!args[0]) {
       rl.setPrompt('请输入项目名')
       rl.prompt()
@@ -54,7 +55,7 @@ switch (args) {
     break
 }
 
-function buildProject (pathName, supportIE) {
+function buildProject(pathName, supportIE) {
 
   fse.ensureDir(pathName, function (error) {
     if (error) {
@@ -62,24 +63,72 @@ function buildProject (pathName, supportIE) {
     } else {
       console.log(`拷贝模板工程。。。`)
       // 将模板工程anu-demo, 复制到新项目中
-       fse.copy(__dirname + '/anu-demo', pathName).then(() => {
+      fse.copy(__dirname + '/anu-demo', pathName).then(() => {
         process.chdir(pathName)
         // 执行npm i
         console.log(`执行 yarn install。。。`)
-       var npm = cp.exec('yarn', {cwd: pathName,stdio: "inherit"}, function (error, stdout, stderr) {
+        let npm = cp.exec('yarn', { cwd: pathName, stdio: "inherit" }, function (error, stdout, stderr) {
           if (error) {
             catchError(error)
-          }else {
+          } else {
             // 执行 webpack 
             console.log(`执行 webpack。。。`)
             let configuration = require(path.resolve(pathName, './webpack.config.js'))
-            webpack(configuration, function (err, stats) {
-              console.log('完工，打开浏览器')
-            })
+            let compiler = webpack(configuration, catchError)
+            let server = new WebpackDevServer(compiler, {
+              // webpack-dev-server options
+
+            //   contentBase: "http://localhost/",
+              // Can also be an array, or: contentBase: "http://localhost/",
+
+              hot: true,
+              // Enable special support for Hot Module Replacement
+              // Page is no longer updated, but a "webpackHotUpdate" message is send to the content
+              // Use "webpack/hot/dev-server" as additional module in your entry point
+              // Note: this does _not_ add the `HotModuleReplacementPlugin` like the CLI option does. 
+
+              // Set this as true if you want to access dev server from arbitrary url.
+              // This is handy if you are using a html5 router.
+              historyApiFallback: false,
+
+              // Set this if you want to enable gzip compression for assets
+              compress: true,
+
+              // Set this if you want webpack-dev-server to delegate a single path to an arbitrary server.
+              // Use "**" to proxy all paths to the specified server.
+              // This is useful if you want to get rid of 'http://localhost:8080/' in script[src],
+              // and has many other use cases (see https://github.com/webpack/webpack-dev-server/pull/127 ).
+    
+
+              // pass [static options](http://expressjs.com/en/4x/api.html#express.static) to inner express server
+              staticOptions: {
+              },
+
+              // webpack-dev-middleware options
+              quiet: false,
+              noInfo: false,
+              lazy: true,
+              filename: "bundle.js",
+              watchOptions: {
+                aggregateTimeout: 300,
+                poll: 1000
+              },
+              // It's a required option.
+              publicPath: "/assets/",
+              headers: { "X-Custom-Header": "yes" },
+              stats: { colors: true }
+            });
+            
+            server.listen(8080, "localhost", function() {
+              console.log("\x1b[36m%s\x1b[0m", "请在浏览器打开 http://localhost:8080")
+            });
+
+
+
           }
         })
-        npm.stdout.on("data", function(data){
-           console.log("输出 "+ data)
+        npm.stdout.on("data", function (data) {
+          console.log("\x1b[35m%s\x1b[0m", data)
         })
         npm.on('close', function (code) {
           if (code !== 0) {
